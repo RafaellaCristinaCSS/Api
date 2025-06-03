598e36c561c26301f01a53c786d042e1b302764d
Rafaella Cristina <rsacramento@osas.com.br>
2025-06-03 10:05:05 -0300

    retornando codigo com adaptacoes

--- Program.cs
+++ Program.cs
@@ -12,29 +12,13 @@ namespace ScholaAi
         {
             var builder = WebApplication.CreateBuilder(args);
             var configuration = builder.Configuration;
-            var key = Encoding.UTF8.GetBytes(configuration["JwtSettings:Secret"]);
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = configuration["JwtSettings:Issuer"],
-                        ValidAudience = configuration["JwtSettings:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(key)
-                    };
-                });
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddCors(options =>
             {
@@ -46,32 +30,30 @@ namespace ScholaAi
                 });
             });
 
-            builder.Services.AddHttpClient("ExternalApi", client =>
-            {
-                client.BaseAddress = new Uri(configuration["ApiBaseUrl"]);
-            });
-
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseCors("AllowGitHubPages");
             app.UseAuthentication();
             app.UseAuthorization();
-
             app.MapControllers();
 
             using (var scope = app.Services.CreateScope())
             {
-                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                db.Database.Migrate();
+                try
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao aplicar migrations: {ex.Message}");
+                    throw;
+                }
             }
-
             app.Run();
         }
     }
